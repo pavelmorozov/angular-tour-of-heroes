086f4d675a6644efefe697c0e36ff3d7e6b66467
pm <pm@pm-node>
2017-10-25 17:09:58 +0300

    Routing added.
    The code become more modular, templates and css are stored in separate
    files.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,24 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
+import { RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
-  ],
-  providers: [],
+    FormsModule,
+    AppRoutingModule],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/heroes.component.ts
@@ -0,0 +1,31 @@
+import {Component, OnInit} from '@angular/core';
+import {Hero} from './hero';
+import {HeroService} from './hero.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-my-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: [ './heroes.component.css' ]
+})
+export class HeroesComponent implements OnInit {
+
+  heroes: Hero[];
+  selectedHero: Hero;
+
+  constructor(private router: Router,
+    private heroService: HeroService) {}
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+  getHeroes(): void {
+    this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
+  }
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
+  }
+}

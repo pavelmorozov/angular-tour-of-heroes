044f8a204fb1a9e240911940195acca4dc9ef54c
Pavlo M <pm@pm-node.none>
2017-10-30 21:52:37 +0200

    http interceptor with CRUD and search implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,12 @@ import { Component } from '@angular/core';
   template: `
     <h1>{{title}}</h1>
 
-    =====
+    = = = = =
     <nav>
       <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
       <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
     <nav>
-    =====
+    = = = = =
     <router-outlet></router-outlet>
 
     `,
--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -13,7 +13,7 @@ import { Hero } from './hero';
 })
 export class HeroDetailComponent implements OnInit {
   @Input() hero: Hero;
-    constructor(
+  constructor(
     private heroService: HeroService,
     private route: ActivatedRoute,
     private location: Location
@@ -25,5 +25,9 @@ export class HeroDetailComponent implements OnInit {
   }
   goBack(): void {
    this.location.back();
+  }
+  save(): void {
+    this.heroService.update(this.hero)
+      .then(() => this.goBack());
   }
 }
